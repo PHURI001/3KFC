6960dc1c056b220388a24247842857cb09a418fc
PHURI001 <phuriwat.chat@gmail.com>
2026-04-23 13:32:06 +0700

    Player & Data

---
+++ Assets/Script/GameData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class GameData : MonoBehaviour
+{
+    public float criticalDamage;
+    public float criticalChance;
+    public float dropChance;
+}
---
+++ Assets/Script/Player/Data_Stats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Data_Stats : MonoBehaviour
+{
+    [Header("Player Data Stats")]
+    public int damage;
+    public float criticalDamage;
+    public float criticalChance;
+    public float dropChance;
+}
