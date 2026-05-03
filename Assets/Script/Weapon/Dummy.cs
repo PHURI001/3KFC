e79382e97d095a65c273caaa20634209e002ac06
Kong5109 <kong8071.kk@gmail.com>
2026-05-03 15:15:09 +0700

    Enemy TakeDamage Animation

--- Assets/Script/Weapon/Dummy.cs
+++ Assets/Script/Weapon/Dummy.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 public class Dummy : MonoBehaviour , ITakeDamage
 {
     public event Action OnDeath;
+    [SerializeField] private string takeDamageAnimation;
     [SerializeField] private MaterialValueLearp materialValue;
     [SerializeField] private MaterialBlink materialBlink;
     [SerializeField] private ParticleSystem deathVFX;
     [SerializeField] private Rigidbody body;
     [SerializeField] private int Health = 300;
 
+    private Animator animator;
     private bool isDeath = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void TakeDamage(Data_Stats damageInfo)
     {
         Health -= damageInfo.damage;
@@ -31,6 +39,7 @@ public class Dummy : MonoBehaviour , ITakeDamage
             Transform playerPos = GameObject.FindAnyObjectByType<Player>().transform;
             Vector3 knockback = (playerPos.position - body.position).normalized;
             body.AddForce(-knockback * 50);
+            animator.Play(takeDamageAnimation);
         }
     }
 }
