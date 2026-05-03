2062e40a8f3c35217c6caec64b576948a6383a29
Kong5109 <kong8071.kk@gmail.com>
2026-05-03 14:20:16 +0700

    Enemy Knockback

--- Assets/Script/Weapon/Dummy.cs
+++ Assets/Script/Weapon/Dummy.cs
@@ -1,21 +1,36 @@
+using System;
 using UnityEngine;
 
 public class Dummy : MonoBehaviour , ITakeDamage
 {
-    public void TakeDamage(Data_Stats damage)
-    {
-        Debug.Log("Take Damage");
-    }
+    public event Action OnDeath;
+    [SerializeField] private MaterialValueLearp materialValue;
+    [SerializeField] private MaterialBlink materialBlink;
+    [SerializeField] private ParticleSystem deathVFX;
+    [SerializeField] private Rigidbody body;
+    [SerializeField] private int Health = 300;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool isDeath = false;
+    public void TakeDamage(Data_Stats damageInfo)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Health -= damageInfo.damage;
+        if (Health < 0 && isDeath == false)
+        {
+            isDeath = true;
+            OnDeath?.Invoke();
+            materialValue?.Play();
+            deathVFX?.Play();
+            Transform playerPos = GameObject.FindAnyObjectByType<Player>().transform;
+            body.freezeRotation = false;
+            Vector3 knockback = (playerPos.position - body.position).normalized;
+            body.AddForce(-knockback * 500);
+        }
+        else
+        {
+            materialBlink?.Play();
+            Transform playerPos = GameObject.FindAnyObjectByType<Player>().transform;
+            Vector3 knockback = (playerPos.position - body.position).normalized;
+            body.AddForce(-knockback * 50);
+        }
     }
 }
