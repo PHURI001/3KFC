89a5132ba62161a125cd866f6111ad89ae0fa820
Kong5109 <kong8071.kk@gmail.com>
2026-05-16 11:54:16 +0700

    Map Data Storage

--- Assets/Script/UI/EndGameUI.cs
+++ Assets/Script/UI/EndGameUI.cs
@@ -4,6 +4,13 @@ public class EndGameUI : MonoBehaviour , IUIState
 {
     [SerializeField] private GameObject endGameUIObj;
 
+    public void OpenUI(float timeToClear,int totalCoinEarn,int totalDamage)
+    {
+        Debug.Log(timeToClear + "timeToClear");
+        Debug.Log(totalCoinEarn + "totalCoinEarn");
+        Debug.Log(totalDamage + "totalDamage");
+    }
+
     public void Enter()
     {
         Debug.Log("Enable");
